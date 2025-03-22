051d85699e0c0835fb6476d3eaac8013a1d83375
fareedhaSamadh <fareedha199@gmail.com>
2025-03-22 21:45:06 +0530

    added new class Constant.java in App Demo Project

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -8,7 +8,7 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("This data "+ data) ;
-		if(data.equals("javatechie")) {
+		if(data.equals(Constant.DATA_TYPE)) {
 			System.out.println("Welcome") ;
 			}
 		}
